585eaead877d787aa9187814e1a6966bc983402f
whyliam <lidzd1992@gmail.com>
2016-04-29 18:49:00 -0400

    去除末尾有换行符

--- splitargs.py
+++ splitargs.py
@@ -13,7 +13,7 @@ def getargs(wf):
     part = int(sys.argv[2])
 
     if part == 1:
-        print query[1]
+        sys.stdout.write(query[1].strip())
     elif part == 2:
         if query[2]:
             bashCommand = "say --voice='Samantha' " + query[2]
