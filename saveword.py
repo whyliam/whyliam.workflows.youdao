3989fce02f87d1877bbcdd4d1705d279681bd452
whyliam <liszd@users.noreply.github.com>
2020-02-01 18:09:07 +0800

    优化 同步到单词本

--- saveword.py
+++ saveword.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-import sys,os
+import sys
+import os
 import re
 import json
-import cookielib, urllib2, urllib
+import cookielib
+import urllib2
+import urllib
 import hashlib
 import logging
 
@@ -14,7 +17,8 @@ sys.setdefaultencoding('utf8')
 
 class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_302(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        result = urllib2.HTTPRedirectHandler.http_error_302(
+            self, req, fp, code, msg, headers)
         result.status = code
         result.headers = headers
         return result
@@ -22,12 +26,13 @@ class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
 
 cookie_filename = 'youdao_cookie'
 fake_header = [
-        ('User-Agent', 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'),
-        ('Content-Type', 'application/x-www-form-urlencoded'),
-        ('Cache-Control', 'no-cache'),
-        ('Accept', '*/*'),
-        ('Connection', 'Keep-Alive'),
-    ]
+    ('User-Agent', 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'),
+    ('Content-Type', 'application/x-www-form-urlencoded'),
+    ('Cache-Control', 'no-cache'),
+    ('Accept', '*/*'),
+    ('Connection', 'Keep-Alive'),
+]
+
 
 class SaveWord(object):
 
@@ -39,7 +44,8 @@ class SaveWord(object):
         self.word = word
         self.cj = cookielib.LWPCookieJar(cookie_filename)
         if os.access(cookie_filename, os.F_OK):
-            self.cj.load(cookie_filename, ignore_discard=True, ignore_expires=True)
+            self.cj.load(cookie_filename, ignore_discard=True,
+                         ignore_expires=True)
         self.opener = urllib2.build_opener(
             SmartRedirectHandler(),
             urllib2.HTTPHandler(debuglevel=0),
@@ -50,24 +56,27 @@ class SaveWord(object):
 
     def loginToYoudao(self):
         self.cj.clear()
-        first_page = self.opener.open('https://account.youdao.com/login?back_url=http://dict.youdao.com&service=dict')
+        first_page = self.opener.open(
+            'https://account.youdao.com/login?back_url=http://dict.youdao.com&service=dict')
         login_data = urllib.urlencode({
-            'app'  : 'web',
-            'tp'  : 'urstoken',
-            'cf'  : '7',
-            'fr'  : '1',
-            'ru'  : 'http://dict.youdao.com',
-            'product'  : 'DICT',
-            'type'  : '1',
-            'um'  : 'true',
-            'username'  : self.username,
-            'password'  : self.password,
-            'savelogin' : '1',
+            'app': 'web',
+            'tp': 'urstoken',
+            'cf': '7',
+            'fr': '1',
+            'ru': 'http://dict.youdao.com',
+            'product': 'DICT',
+            'type': '1',
+            'um': 'true',
+            'username': self.username,
+            'password': self.password,
+            'savelogin': '1',
         })
-        response = self.opener.open('https://logindict.youdao.com/login/acc/login', login_data)
+        response = self.opener.open(
+            'https://logindict.youdao.com/login/acc/login', login_data)
         if response.headers.get('Set-Cookie') != None:
             if response.headers.get('Set-Cookie').find(self.username) > -1:
-                self.cj.save(cookie_filename, ignore_discard=True, ignore_expires=True)
+                self.cj.save(cookie_filename, ignore_discard=True,
+                             ignore_expires=True)
                 return True
             else:
                 return False
@@ -76,28 +85,31 @@ class SaveWord(object):
 
     def syncToYoudao(self):
         post_data = urllib.urlencode({
-            'word' : self.word.get('word'),
-            'phonetic' : self.word.get('phonetic'),
+            'word': self.word.get('word'),
+            'phonetic': self.word.get('phonetic'),
             'desc': self.word.get('trans'),
-            'tags' : self.word.get('tags'),
+            'tags': self.word.get('tags'),
         })
         self.opener.addheaders = fake_header + [
             ('Referer', 'http://dict.youdao.com/wordbook/wordlist'),
         ]
-        response = self.opener.open('http://dict.youdao.com/wordbook/wordlist?action=add', post_data)
+        response = self.opener.open(
+            'http://dict.youdao.com/wordbook/wordlist?action=add', post_data)
         return response.headers.get('Location') == 'http://dict.youdao.com/wordbook/wordlist'
 
     def generateWordBook(self, source_xml):
         item = self.word
         item_xml = '<item>'
         for i in item:
-            value = '<![CDATA[' + item[i] + ']]>' if i in ["trans", "phonetic"] else item[i]
+            value = '<![CDATA[' + item[i] + ']]>' if i in ["trans",
+                                                           "phonetic"] else item[i]
             item_xml = item_xml + '<' + i + '>' + value + '</' + i + '>\n'
         item_xml = item_xml + '</item>\n'
 
-        source_xml = re.sub('<item>(?:(?!<\/item>)[\s\S])*<word>'+ item.get("word") +'<\/word>[\s\S]*?<\/item>\n', '', source_xml)
+        source_xml = re.sub('<item>(?:(?!<\/item>)[\s\S])*<word>' + item.get(
+            "word") + '<\/word>[\s\S]*?<\/item>\n', '', source_xml)
         if source_xml.find('</wordbook>') > -1:
-            source_xml = source_xml.replace('</wordbook>','') + item_xml
+            source_xml = source_xml.replace('</wordbook>', '') + item_xml
         else:
             source_xml = '<wordbook>\n' + item_xml
         return source_xml + '</wordbook>'
@@ -106,13 +118,13 @@ class SaveWord(object):
         try:
             source_xml = ''
             if os.path.exists(self.localfile):
-                f = open(self.localfile,'r')
+                f = open(self.localfile, 'r')
                 source_xml = f.read()
                 f.close()
-            f = open(self.localfile,'w')
+            f = open(self.localfile, 'w')
             f.write(self.generateWordBook(source_xml))
             f.close()
-        except Exception,e:
+        except Exception, e:
             return e
         return 0
 
@@ -123,14 +135,19 @@ class SaveWord(object):
             result = self.saveLocal()
             sys.stdout.write('帐号出错，临时保存至本地单词本')
 
+
 if __name__ == '__main__':
     params = sys.argv[1].split('$%')
+    version = params[0]
+    query = params[1]
+    result = params[2]
 
     username = os.getenv('username', '').strip()
     password = os.getenv('password', '').strip()
     filepath = os.getenv('filepath', '').strip()
     if filepath is None:
-        filepath = os.path.join(os.environ['HOME'] , 'Documents/Alfred-youdao-wordbook.xml')
+        filepath = os.path.join(
+            os.environ['HOME'], 'Documents/Alfred-youdao-wordbook.xml')
     else:
         filepath = os.path.expanduser(filepath)
 
@@ -139,13 +156,14 @@ if __name__ == '__main__':
     password_md5 = m2.hexdigest()
 
     item = {
-        "word" : params[0],
-        "trans" : params[1],
-        "tags" : "Alfred",
-        "progress" : "-1",
+        "word": query,
+        "trans": result,
+        # "phonetic":
+        "tags": "Alfred",
+        "progress": "-1",
     }
 
-    saver = SaveWord(username, password_md5 , filepath, item)
+    saver = SaveWord(username, password_md5, filepath, item)
 
     wf = Workflow3()
     sys.exit(wf.run(saver.save))
--- splitargs.py
+++ splitargs.py
@@ -8,90 +8,86 @@ from workflow import Workflow3
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
 def escape_quote(result):
     return result.replace("\'", "\\\'").replace("\"", "\\\"")
+
 
 def get_args(wf):
     query = sys.argv[1]
     arg_array = query.split('$%')
     command = str(sys.argv[2])
 
-# try:
-    version = arg_array[0]
-    query = arg_array[1]
-    result = arg_array[2]
-    query_language = arg_array[3]
-    pronounce = arg_array[4]
-    operation = arg_array[5]
+    try:
+        version = arg_array[0]
+        query = arg_array[1]
+        result = arg_array[2]
+        query_language = arg_array[3]
+        pronounce = arg_array[4]
+        operation = arg_array[5]
 
-    # 是否有更新
-    if operation == 'update':
-        wf.start_update()
+        # 是否有更新
+        if operation == 'update':
+            wf.start_update()
 
-    # 是否有错误
-    elif operation == 'error':
-        import webbrowser
-        url = "https://blog.naaln.com/2017/04/alfred-youdao-intro/"
-        webbrowser.open(url)
-        return
+        # 是否有错误
+        elif operation == 'error':
+            import webbrowser
+            url = "https://blog.naaln.com/2017/04/alfred-youdao-intro/"
+            webbrowser.open(url)
+            return
 
-    # 查询的单词
-    if command == 'search':
-        sys.stdout.write(query)
+        # 查询的单词
+        if command == 'search':
+            sys.stdout.write(query)
 
-    # 翻译的结果
-    elif command == 'copy':
-        sys.stdout.write(result)
+        # 翻译的结果
+        elif command == 'copy':
+            sys.stdout.write(result)
 
-    # 发音
-    elif command == "pronounce":
-        wf.logger.debug('===============')
-        wf.logger.debug(query_language)
-        if ('2') in query_language:
-            data_form, data_to = query_language.split('2')
-        else:
-            data_form = query_language
-            data_to = ''
+        # 发音
+        elif command == "pronounce":
+            if ('2') in query_language:
+                data_form, data_to = query_language.split('2')
+            else:
+                data_form = query_language
+                data_to = ''
 
-        wf.logger.debug(data_form)
-        wf.logger.debug(data_to)
-        wf.logger.debug(pronounce)
-        if data_form == 'EN':
-            if pronounce == '':
-                pronounce = query
-            wf.logger.debug(pronounce)
-            bashCommand = "say --voice='Samantha' " + escape_quote(pronounce)
-            wf.logger.debug(bashCommand)
-            os.system(bashCommand)
+            if data_form == 'EN':
+                if pronounce == '':
+                    pronounce = query
+                bashCommand = "say --voice='Samantha' " + \
+                    escape_quote(pronounce)
+                os.system(bashCommand)
 
-        elif data_form == 'KO':
-            if pronounce == '':
-                pronounce = query
-            bashCommand = "say --voice='Yuna' " + escape_quote(pronounce)
-            os.system(bashCommand)
+            elif data_form == 'KO':
+                if pronounce == '':
+                    pronounce = query
+                bashCommand = "say --voice='Yuna' " + escape_quote(pronounce)
+                os.system(bashCommand)
 
-        elif data_form == 'zh-CHS':
-            if pronounce == '':
-                pronounce = query
-            bashCommand = "say --voice='Ting-Ting' " + escape_quote(pronounce)
-            os.system(bashCommand)
+            elif data_form == 'zh-CHS':
+                if pronounce == '':
+                    pronounce = query
+                bashCommand = "say --voice='Ting-Ting' " + \
+                    escape_quote(pronounce)
+                os.system(bashCommand)
 
-        elif data_form == 'JA':
-            if pronounce == '':
-                pronounce = query
-            bashCommand = "say --voice='Kyoko' " + escape_quote(pronounce)
-            os.system(bashCommand)
+            elif data_form == 'JA':
+                if pronounce == '':
+                    pronounce = query
+                bashCommand = "say --voice='Kyoko' " + escape_quote(pronounce)
+                os.system(bashCommand)
 
-        elif data_to == 'EN':
-            if pronounce == '':
-                pronounce = result
-            bashCommand = "say --voice='Samantha' " + escape_quote(pronounce)
-            os.system(bashCommand)
+            elif data_to == 'EN':
+                if pronounce == '':
+                    pronounce = result
+                bashCommand = "say --voice='Samantha' " + \
+                    escape_quote(pronounce)
+                os.system(bashCommand)
 
-    # except Exception as e:
-    #     return
-
-
+        except Exception as e:
+            return
 
 
 if __name__ == '__main__':
